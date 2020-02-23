624cb8e3c0a2a4d98e8b018b484b36c2402e1f2a
Elmakhlouk Chouaib <elchouai@e2r6p5.1337.ma>
2020-02-23 15:47:07 +0100

    d

--- readdstrem.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-
-var myReadStream = fs.createReadStream(__dirname + '/README.md');
-myReadStream.on('data', function(chunk){
-    console.log('new chunk received');
-    console.log(chunk);
-});
